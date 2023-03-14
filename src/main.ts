d054b77087a9510f2f83b98f6b1d3317c11d9a51
George Bockari <bitwhys@gmail.com>
2023-03-14 01:09:40 -0400

    chore: initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+// import { youtube } from '@googleapis/youtube'
+
+// const client = await youtube({
+//   version: 'v3',
+//   auth: '',
+// })
+
+console.log(process.env.GOOGLE_YOUTUBE_API_KEY)
