d261e25b2bb62a953dd8376a63635614acf69d1f
George Bockari <bitwhys@gmail.com>
2023-03-14 01:27:34 -0400

    refactor: can get a response for a video by id

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config'
-// import { youtube } from '@googleapis/youtube'
+import { youtube } from '@googleapis/youtube'
 
-// const client = await youtube({
-//   version: 'v3',
-//   auth: '',
-// })
+const client = await youtube({
+  version: 'v3',
+  auth: process.env.GOOGLE_YOUTUBE_API_KEY,
+})
 
-console.log(process.env.GOOGLE_YOUTUBE_API_KEY)
+const response = await client.videos.list({
+  part: ['snippet'],
+  id: ['11jpa8e5jEQ'],
+})
+
+console.log(response)
