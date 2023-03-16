814184b15647bb33c1530deb67928bd419c19e17
George Bockari <bitwhys@gmail.com>
2023-03-15 21:05:24 -0400

    refactor: revert to initial state of nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { CommentsController } from './comments/comments.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController, CommentsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
